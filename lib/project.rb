1c5965fbb68c6b2398f6e5ceaa71d1cb5b2e5f75
lian <meta.rb@gmail.com>
2008-10-24 13:51:15 +0200

    project.rb

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,42 @@
 class WebProject
-  # ..
+  attr_accessor :path, :meta
+  def initialize(path)
+    @path = path
+    init_config
+  end
+  
+  def init_config
+    @meta ||= {}
+    @meta = YAML.load_file("#{@path}/project.meta")
+  end
+end
+
+class WebProject
+  DIR_STRUCTURE = ["","pages","pages/_layout","pages/_partial","resources","resources/css","resources/js","resources/media"]
+  FILE_STRUCTURE = ["pages/index.erb","resources/css/default.css","resources/js/default.js"]
+  
+  def self.generate_default_files_and_directories(project_path)
+    DIR_STRUCTURE.each  { |dir_path| tmp_path = "#{project_path}/#{dir_path}"; Dir.mkdir(tmp_path) unless File.exists?(tmp_path) }
+    FILE_STRUCTURE.each { |file_path| tmp_path = "#{project_path}/#{file_path}"; File.open(tmp_path,"wb") { |f| f.print "" } unless File.exists?(tmp_path) }
+    project_meta = { 'project_title' => File.basename(project_path), 'project_type' => 'web' }
+    File.open(project_path+"/project.meta","wb") { |f| f.print project_meta.to_yaml }
+    self.initialize_git(project_path, project_meta)
+    [project_path, project_meta]
+  end
+
+  def self.initialize_git(path,meta)
+    unless File.exists?(path+"/.git")
+      Dir.chdir(path)
+      system "git init > /dev/null 2>&1"
+      system "git add project.meta > /dev/null 2>&1"
+      system "git commit -m 'init #{meta['project_title']} project' > /dev/null 2>&1"
+    end
+  end
+
+  def self.create(name)
+    project_path = Webmate.projects_path+"/#{name}"
+    new_path, meta = self.generate_default_files_and_directories(project_path) unless File.exists?(project_path)
+    project_path = new_path if new_path
+    new project_path
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,5 +1,41 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
+
 describe WebProject do
-  it "does your work."
+  describe "when created" do
+    it "should create default files and directory structure" do
+      project_name = "rspec_create_files_and_directories"
+      project_path = Webmate.projects_path+"/#{project_name}"
+      remove_rspec_dir(project_path)
+      project = WebProject.create project_name
+      
+      project.class.should == WebProject
+      (WebProject::DIR_STRUCTURE + WebProject::FILE_STRUCTURE).each do |path|
+        File.exists?(project_path+"/#{path}").should == true
+      end
+    end
+    
+    it "should create git repository of project" do
+      project_name = "rspec_create_git_repo"
+      project_path = Webmate.projects_path+"/#{project_name}"
+      remove_rspec_dir(project_path)
+      project = WebProject.create project_name
+      
+      File.exists?(project_path+"/.git").should == true
+    end
+  end
+  
+  describe "when initialized" do
+    before(:each) do
+      @project_name = "rspec_project_initialized"
+      @project_path = Webmate.projects_path+"/#{@project_name}"
+      remove_rspec_dir(@project_path)
+    end
+    
+    it "should return directory path of project" do
+      @project = WebProject.create @project_name
+      @project.path.should == @project_path
+    end
+  end
+  
 end
